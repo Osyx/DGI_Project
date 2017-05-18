639ac3d97b5388bb60dcf7631ef2cf3b2192ef35
Osyx <ofalkman@gmail.com>
2017-05-18 19:00:43 +0200

    Started over from the beginning. Let's do this correctly.

--- DGI_Project/main.cpp
+++ DGI_Project/main.cpp
@@ -1,2 +1,62 @@
-#include <GL/glut.h>
+#define GLEW_STATIC
+#include <GL/glew.h>
+//#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
 
+int main()
+{
+	glfwInit();
+	
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr); // Windowed
+	//GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", glfwGetPrimaryMonitor(), nullptr); // Fullscreen
+
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+			glfwSetWindowShouldClose(window, GL_TRUE);
+
+	}
+
+	GLuint vertexBuffer;
+	glGenBuffers(1, &vertexBuffer);
+
+	std::printf("%u\n", vertexBuffer);
+
+	glfwTerminate();
+	return 0;
+}
+
+void drawTriangle() {
+	float vertices[] = {
+		 0.0f,  0.5f, // Vertex 1 (X, Y)
+		 0.5f, -0.5f, // Vertex 2 (X, Y)
+		-0.5f, -0.5f  // Vertex 3 (X, Y)
+	};
+
+	GLuint vbo; // Create a Vertex buffer index
+	glGenBuffers(1, &vbo); // Generate 1 buffer
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo); // Make buffer active
+
+	// GL_STATIC_DRAW -> Upload once, never change, GL_DYNAMIC_DRAW -> Created once but changed sometimes, drawn a lot more than that, GL_STREAM_DRAW -> Uploaded once, drawn once
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // Transfer data onto buffer, thus send it to the graphics card
+
+
+
+}
